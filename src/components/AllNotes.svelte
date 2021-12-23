9cbe99842e03c9d526f3cc418eb307c52db1e46c
elikya <elikyabokanga@gmail.com>
2021-12-23 06:17:01 -0500

    modal complete

--- src/components/AllNotes.svelte
+++ src/components/AllNotes.svelte
@@ -2,8 +2,9 @@
   import { Link } from "svelte-routing"
   import Bar from "./Bar.svelte"
   import { ModalCard } from "svelma"
-  import SingleNote from "./SingleNote.svelte"
   export let notes = []
+
+  let active = false
 </script>
 
 <div>
@@ -11,13 +12,23 @@
   {#each notes as note}
     <div class="block">
       <Link to="{'/notes/'+note.id}"><h1 class="title">{note.title}</h1></Link>
-      <h2>{note.body}</h2>
-      <div class="modal">
-        <div class="modal-background"></div>
-        <div class="modal-content">
-          <SingleNote/>
+      {#if note.body.length > 140}
+        <h2 on:click={()=>{active=!active}}>{note.body.slice(0,139)+"..."}</h2>
+        <div class="modal {active? 'is-active': ''}">
+          <div class="modal-background"></div>
+          <div class="modal-card">
+            <header class="modal-card-head">
+              <p class="modal-card-title">{note.title}</p>
+              <button class="delete" aria-label="close" on:click={()=>{active=!active}}/>
+            </header>
+            <section class="modal-card-body">
+              <h2>{note.body}</h2>
+            </section>
+          </div>
         </div>
-      </div>
+      {:else}
+        <h2>{note.body}</h2>
+      {/if}
     </div>
   {/each}
 </div>
--- src/components/Bar.svelte
+++ src/components/Bar.svelte
@@ -9,12 +9,9 @@
             navigate("/search/"+term, {replace: true})
         }
     }
-
-    // <input type="text" placeholder="search" bind:value={term}/>
-    // <Button type="is-dark" nativeType="submit">Search</Button>
 </script>
 
-<div>
+<div class="block">
     <form class="field has-addons" on:submit|preventDefault={handleSubmit}>
         <div class="control is-expanded" id="bar"><input class="input" type="text" placeholder="search" bind:value={term}/></div>
         <div class="control"><button class="button is-dark is-outlined" type="submit">Search</button></div>
@@ -22,5 +19,4 @@
 </div>
 
 <style>
-    /* #bar {max-width:80vw; margin: 0 auto;} */
 </style>
