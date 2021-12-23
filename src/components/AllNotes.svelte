4ca8421efb3b692222a7c802fa18e6bc0e5f6931
elikya <elikyabokanga@gmail.com>
2021-12-23 05:43:54 -0500

    modal changes

--- src/components/AllNotes.svelte
+++ src/components/AllNotes.svelte
@@ -1,6 +1,8 @@
 <script>
   import { Link } from "svelte-routing"
   import Bar from "./Bar.svelte"
+  import { ModalCard } from "svelma"
+  import SingleNote from "./SingleNote.svelte"
   export let notes = []
 </script>
 
@@ -10,6 +12,12 @@
     <div class="block">
       <Link to="{'/notes/'+note.id}"><h1 class="title">{note.title}</h1></Link>
       <h2>{note.body}</h2>
+      <div class="modal">
+        <div class="modal-background"></div>
+        <div class="modal-content">
+          <SingleNote/>
+        </div>
+      </div>
     </div>
   {/each}
 </div>
